2d11a79488dcbdb0745ba3014c4701d83a79c1c3
Oleksii Muhyr <1.2.aznch@gmai.com>
2024-06-03 16:27:16 +0300

    delete redundant comments

--- city/schemas.py
+++ city/schemas.py
@@ -1,24 +1,3 @@
-# from pydantic import BaseModel
-#
-#
-# class CityBase(BaseModel):
-#     name: str
-#     additional_info: str
-#
-#
-# class City(CityBase):
-#     id: int
-#
-#     class Config:
-#         from_attributes = True
-#
-#
-# class CityCreate(CityBase):
-#     pass
-#
-#
-# class CityUpdate(CityBase):
-#     pass
 from pydantic import BaseModel
 
 
